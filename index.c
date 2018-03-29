966870cd9621b3f77e3aca1cec87526135c26493
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-29 15:05:29 +0200

    Als argc groter is dan 1 de dingen die je na de comando kan uitvoeren

--- index.c
+++ index.c
@@ -1,3 +1,12 @@
 //libs importeren
 //Invoer uitvoer lib 
 #include <stdio.h>
+
+//Main functie
+int main(int argc, char **argv)
+{
+    //Als argc groter is dan 1 de dingen die je na de comando kan uitvoeren
+    if (argc > 1) {
+        printf("test \n");
+    }
+}
