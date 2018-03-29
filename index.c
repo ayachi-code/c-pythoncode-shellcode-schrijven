2b2f78ac8856b45997389ead3bf858a8a989749a
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-29 15:10:54 +0200

    Alle paramters na de commando worden geloopt en dan geprint...

--- index.c
+++ index.c
@@ -7,7 +7,11 @@ int main(int argc, char **argv)
 {
     //Als argc groter is dan 1 de dingen die je na de comando kan uitvoeren
     if (argc > 1) {
-        printf("test \n");
+        int i;
+        //Alle paramters na de commando worden geloopt en dan geprint...
+        for (i = 1; i < argc; i++) {
+            printf("%s\n",argv[i]);
+        }
     } else {
         //Als de gene geen paramter toevoegt dan word deze bericht onder uitgevoerd...
         printf("Welkom bij ps schrijfer type --help om alle opties te tonen ... \n");
