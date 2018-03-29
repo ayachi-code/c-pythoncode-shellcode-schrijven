b098b2234227527743167ddab4a848cccaecc420
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-29 19:54:58 +0200

    String bewerking module geimporteerd

--- index.c
+++ index.c
@@ -1,6 +1,9 @@
 //libs importeren
 //Invoer uitvoer lib 
 #include <stdio.h>
+//String bewerking module geimporteerd
+#include <string.h>
+
 
 //Main functie
 int main(int argc, char **argv)
@@ -12,6 +15,7 @@ int main(int argc, char **argv)
         for (i = 1; i < argc; i++) {
             //Char variable gemaakt met --help als inhoud
             char *help = "--help";
+
         }
     } else {
         //Als de gene geen paramter toevoegt dan word deze bericht onder uitgevoerd...
